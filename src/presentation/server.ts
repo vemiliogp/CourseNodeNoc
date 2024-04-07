39a39bfc98362d17eefe656968b7013717cb1254
Victor E González P <vemiliogp@gmail.com>
2024-04-07 16:29:37 +0200

    First commit

---
+++ src/presentation/server.ts
@@ -0,0 +1,16 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CronService } from "./cron/cron-service";
+
+export class Server {
+  public static start() {
+    console.log("Server started...");
+
+    CronService.createJob("*/5 * * * * *", () => {
+      const url = "https://google.com";
+      new CheckService(
+        () => console.log(`${url} is ok`),
+        (error) => console.error(error)
+      ).execute(url);
+    });
+  }
+}
